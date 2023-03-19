c6159b987e16e03d61d8ce41550efd695658050d
Muhammad Abdullah <muhammad.abdullah@u.yale-nus.edu.sg>
2023-03-19 18:59:08 +0800

    catalogue with the man himself Jakob

--- frontend_react/src/Components/Body/Categories/ProductCatalogue/ProductCatalogue.js
+++ frontend_react/src/Components/Body/Categories/ProductCatalogue/ProductCatalogue.js
@@ -20,27 +20,144 @@ export default function ProductCatalogue() {
       description: "A very good powder",
       image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
       quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    }, 
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    },
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    }, 
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    },
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    }, 
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    },
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
+    }, 
+    {
+      price: 10,
+      title:
+        "Surf Excel Detergent Soap Bar Stain Eraser with Fragrance Surf Excel Detergent Soap Bar Stain Eraser",
+      description: "A very good powder",
+      image:
+        "https://sg.jimpus.com/wp-content/uploads/2020/07/61ed7Ccc8L._SL1000_.jpg", 
+      quantity: "0.75 kg | $1 per kg"
+    }, 
+    {
+      price: 12,
+      title: "ARIEL Laundry Powder Detergent 500G Aroma Original 3-Pack. ",
+      description: "A very good powder",
+      image: "https://m.media-amazon.com/images/I/615chcqv1vL.jpg", 
+      quantity: "1 kg | $12 per kg"
     }
+
   ];
 
   const containerStyles = {
     display: "flex", 
-    border: "1px solid blue",
+    border: "3px solid blue",
     position: "relative",
     height: "100%",
-    width: "100%"
+    width: "100%",
+    flexWrap: "wrap",
   };
+
+  const productContainers = [];
+
+  for (let i = 0; i < products.length; i++) {
+    productContainers.push(
+      <div key={i}>
+        <ProductContainer product={products[i]} />
+      </div>
+    );
+  }
 
   return (
     <div style={containerStyles}>
-      <div>
-        {" "}
-        <ProductContainer product={products[0]} />{" "}
-      </div>
-      <div>
-        {" "}
-        <ProductContainer product={products[1]} />{" "}
-      </div>
+      {productContainers}
     </div>
   );
 }
