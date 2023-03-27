92e96f5ff5f2ff43e698f123d79e19b7a85ca769
Pache <111343760+Pache47@users.noreply.github.com>
2023-03-28 05:42:47 +0800

    modified "get_all_products" and added "search_products"

--- backend/api_be.py
+++ backend/api_be.py
@@ -1,20 +1,75 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify, json
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 
+# Custom JSON encoder to handle ObjectId
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return super(JSONEncoder, self).default(o)
+
+app.json_encoder = JSONEncoder
+
 # set up the MongoDB connection
-client = MongoClient('mongodb://localhost:27017/')
-db = client['mydatabase']
-collection = db['mycollection']
+client = MongoClient('mongodb://localhost:27017')
+db = client['egrocery']
+collection = db['inventory']
+
+# API endpoint for retrieving all list of products
+@app.route('/products', methods=['GET'])
+def get_all_products():
+    all_products = collection.find({})
+    return jsonify(list(all_products))
 
 # API endpoint for retrieving filtered list of products
-@app.route('/products', methods=['GET'])
-def get_products():
-    filter_object = request.json  # assume filter object is passed in as JSON
-    #print(request.json) # print the JSON object (debugging)
-    filtered_products = collection.find(filter_object)
+# @app.route('/product/search', methods=['GET'])
+# def get_searched_products():
+#     filter_object = request.json  # assume filter object is passed in as JSON
+#     #print(request.json)#args.get('search_key', None)) # print the JSON object (debugging)
+#     filtered_products = collection.find({'name': 'Tesco Fun Animal Beach Towel'})
+#     return jsonify(list(filtered_products))
+
+
+# # API endpoint for searching products by their name, brand, (price, availability, and other attributes)
+@app.route('/product/search_v2', methods=['POST'])
+def search_products():
+    filter_object = request.json
+    search_name = filter_object.get('name', None)
+    search_brand = filter_object.get('brand', None)
+    search_availability = filter_object.get('availability', None)
+    min_price = filter_object.get('min_price', None)
+    max_price = filter_object.get('max_price', None)
+    search_currency = filter_object.get('currency', None)
+
+    search_filter = {}
+
+    if search_name:
+        search_filter['name'] = {'$regex': search_name, '$options': 'i'}
+
+    if search_brand:
+        search_filter['brand'] = {'$regex': search_brand, '$options': 'i'}
+
+    if search_availability:
+        search_filter['availability'] = search_availability
+
+    if min_price is not None or max_price is not None:
+        price_filter = {}
+        if min_price is not None:
+            price_filter['$gte'] = float(min_price)
+        if max_price is not None:
+            price_filter['$lte'] = float(max_price)
+        search_filter['price'] = price_filter
+
+    if search_currency:
+        search_filter['currency'] = search_currency
+
+    filtered_products = collection.find(search_filter)
+
     return jsonify(list(filtered_products))
+
 
 @app.route('/categories', methods=['GET'])
 def get_categories():
@@ -44,7 +99,7 @@ def get_cart():
         # get the product information from the MongoDB database
         product_info = collection.find_one({'id': item['product_id']})
         # check if the item is available
-        if product_info['stock'] >= item['quantity']:
+        if product_info['quantity'] >= item['quantity']:
             item['available'] = True
             available_products.append(item)
         else:
@@ -63,10 +118,10 @@ def update_database():
         # get the product information from the MongoDB database
         product_info = collection.find_one({'id': item['product_id']})
         # check if the item is available
-        if product_info['stock'] >= item['quantity']:
+        if product_info['quantity'] >= item['quantity']:
             # update the product stock in the database
-            new_stock = product_info['stock'] - item['quantity']
-            collection.update_one({'id': item['product_id']}, {'$set': {'stock': new_stock}})
+            new_stock = product_info['quantity'] - item['quantity']
+            collection.update_one({'id': item['product_id']}, {'$set': {'quantity': new_stock}})
             # add the purchased item to the list
             purchased_items.append(item)
     # return the list of purchased items
@@ -74,4 +129,4 @@ def update_database():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
