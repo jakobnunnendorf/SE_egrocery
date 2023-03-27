5f61065c1ee65fa29547bf8aa6c9545f372c1bef
Joshua Vargas <65795798+vargaseous@users.noreply.github.com>
2023-03-27 11:05:07 +0800

    Add footer component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Body from './Components/Body/Body';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
