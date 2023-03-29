c9b34a47f030713532afcf47c9f394ff6ce61bac
“Jakob <jnunnendorf@icloud.com>
2023-03-29 19:17:55 +0800

    save progress

--- frontend_react/src/Components/Body/Products/FilterBar/FilterBar.js
+++ frontend_react/src/Components/Body/Products/FilterBar/FilterBar.js
@@ -26,7 +26,6 @@ export default function FilterBar(props) {
       }
       const category_filters = ["Meat", "Vegetables", "Fruits", "Bakery", "Dairy", "Drinks", "Snacks", "Other"]
       const price_filters = ["0-3 $", "3-5 $", "5-10 $", "10-20 $", ">20 $"]
-      const random_filters = ["random marc 1", "random marc 2", "random marc 3"]
 
   return (
     <div style={styles.FilterBar_frame}>
@@ -39,11 +38,6 @@ export default function FilterBar(props) {
         <section>
           <h5>Price</h5>
           {price_filters.map((filter, index) => (<Filter type="price" key={index} name={filter} addFilters={props.addFilters} active={props.activeFilters.includes(filter) ? true : false}/>))}
-        </section>
-
-        <section>
-          <h5>random filters</h5>
-          {random_filters.map((filter, index) => (<Filter key={index} name={filter} addFilters={props.addFilters}/>))}
         </section>
 
     </div>
