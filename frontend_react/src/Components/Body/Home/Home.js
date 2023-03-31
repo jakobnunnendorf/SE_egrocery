5b1eebb82dca12973928922da1a42e6872addb1b
“Jakob <jnunnendorf@icloud.com>
2023-03-31 15:08:57 +0800

    initialise home page

--- frontend_react/src/Components/Body/Home/Home.js
+++ frontend_react/src/Components/Body/Home/Home.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="home__banner">
+        <h1 className="home__title">Welcome to eGrocery</h1>
+        <p className="home__subtitle">Shop online for fresh groceries and more</p>
+      </div>
+      <div className="home__categories">
+        {/* Add your categories here as divs or other components */}
+      </div>
+    </div>
+  );
 }
