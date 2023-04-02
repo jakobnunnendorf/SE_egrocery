b0713dc62ad696472f2e299638a58176c38a8709
Pache <tsegayilmaget47@gmail.com>
2023-04-03 00:20:02 +0800

    abdullah magic

--- frontend_react/src/Components/Body/AccountPage/MyAccount/AdminPanel/AdminPanel.js
+++ frontend_react/src/Components/Body/AccountPage/MyAccount/AdminPanel/AdminPanel.js
@@ -1,27 +1,10 @@
 import React, { useState } from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faQuestionCircle, faUserShield, faCloudUploadAlt, faFile} from '@fortawesome/free-solid-svg-icons'
+import "./AdminPanel.css";
 export default function AdminPanel() {
   const [jsonData, setJsonData] = useState([]);
 
-  const styles = {
-    frame: {
-      display: "flex",
-      width: "100%",
-      height: "500px",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "space-around",
-    },
-    input_field: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-around",
-      alignItems: "center",
-      width: "500px",
-      height: "150px",
-      marginBottom: "100px",
-    },
-  };
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -58,10 +41,25 @@ export default function AdminPanel() {
 
 
   return (
-    <div style={styles.frame}>
-      <h1>Admin Panel</h1>
-      <div style={styles.input_field}>
-        <h3>Upload the updated inventory as csv</h3>
+    <div> 
+      <h1> <FontAwesomeIcon icon={faUserShield} size="2x" /></h1> 
+      <h2>Admin Panel</h2>
+    <div className="frame"> 
+      <div className="instructionsFrame"> 
+        <h4> <FontAwesomeIcon icon={faQuestionCircle} /> Instructions </h4>
+        <p>1. Open the Excel sheet containing the product inventory.</p>
+        <p>2. Review and update the product information as needed.</p>
+        <p>3. Save the Excel sheet as a CSV (Comma Separated Values) file.</p>
+        <p>4. Navigate to the upload section of the admin panel on the website.</p>
+        <p>5. Click the "Choose File" button and select the CSV file from your computer.</p>
+        <p>6. Click the "Upload" button to upload the CSV file to the system.</p>
+        <p>7. Wait for the system to process the file and update the product inventory.</p>
+        <p>8. Verify that the product inventory has been updated with the new information.</p>
+    </div>
+      <div className="input_field">
+        <h4> <FontAwesomeIcon icon={faCloudUploadAlt} /> Upload Updated Inventory as csv </h4>
+        <span> 
+        <FontAwesomeIcon icon={faFile} /> &nbsp;
         <input
           type="file"
           id="fileInput"
@@ -69,7 +67,10 @@ export default function AdminPanel() {
           accept=".csv"
           onChange={handleFileUpload}
         />
+        </span>
+        <div className="successfulUpload"> {jsonData.length > 0 && <p> <FontAwesomeIcon icon={faCheckCircle} /> Uploaded Successfully!</p>}</div>
       </div>
+    </div>
     </div>
   );
 }
