20dbd802196c83c69bcc1c01268a2f69d819710e
“Jakob <jnunnendorf@icloud.com>
2023-04-02 20:47:35 +0800

    frontend logic to convert csv to json works. Now we can seamlessly pass the whole csv file as an array of json objects to the backend.

--- frontend_react/src/Components/Body/AccountPage/MyAccount/AdminPanel/AdminPanel.js
+++ frontend_react/src/Components/Body/AccountPage/MyAccount/AdminPanel/AdminPanel.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function AdminPanel() {
+  const [jsonData, setJsonData] = useState([]);
+
   const styles = {
     frame: {
-      display: 'flex',
-      width: '100%',
-      height: '500px',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-around',
+      display: "flex",
+      width: "100%",
+      height: "500px",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "space-around",
     },
     input_field: {
       display: "flex",
@@ -17,15 +19,56 @@ export default function AdminPanel() {
       alignItems: "center",
       width: "500px",
       height: "150px",
-      marginBottom: "100px"
+      marginBottom: "100px",
+    },
+  };
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const csvData = event.target.result;
+      const jsonData = csvToJson(csvData);
+      setJsonData(jsonData);
+      console.log(jsonData);
+    };
+    reader.readAsText(file);
+  };
+
+  const csvToJson = (csvData) => {
+    const lines = csvData.split("\n");
+    const headers = lines[0].split(",");
+    const jsonData = [];
+
+    for (let i = 1; i < lines.length; i++) {
+      const lineData = lines[i].split(",");
+
+      // Check if the line has the same number of columns as the headers
+      if (lineData.length === headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          obj[headers[j].trim()] = lineData[j] ? lineData[j].trim() : "";
+        }
+        jsonData.push(obj);
+      }
     }
-  }
+    return jsonData;
+  };
+
+
   return (
     <div style={styles.frame}>
       <h1>Admin Panel</h1>
       <div style={styles.input_field}>
         <h3>Upload the updated inventory as csv</h3>
-        <input type="file" id="fileInput" name="fileInput" accept=".csv" />
+        <input
+          type="file"
+          id="fileInput"
+          name="fileInput"
+          accept=".csv"
+          onChange={handleFileUpload}
+        />
       </div>
     </div>
   );
