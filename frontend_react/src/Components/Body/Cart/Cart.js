e4a66aa2b2045c778b48c0154ecf540f77c6e1d4
“Jakob <jnunnendorf@icloud.com>
2023-04-02 16:35:46 +0800

    Stripe is connected in general now but the input field is still tiny as fuck. So we have to change it later.

--- frontend_react/src/Components/Body/Cart/Cart.js
+++ frontend_react/src/Components/Body/Cart/Cart.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './Cart.css';
+// add the stripe imports
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { CardElement } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { CheckoutForm } from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_live_51Ms3VvDmRvnpQ6e6h0qFd928Zf6STt5VmgfUNBGg5Q3x5ZPfl9JZ5Gv9TadDXZtNSgpO8oCInSsbszxRiAeC4P2S00QXy3THcP"
+);
+
 
 export default function Cart(props) {
+
+const [checkoutModalOpen, setCheckoutModalOpen] = useState(false);
+
   const totalPrice = props.cartProp.reduce(
     (accumulator, item) => accumulator + item.productInfo.price * item.quantity,
     0
@@ -24,14 +38,28 @@ export default function Cart(props) {
           {props.cartProp.map((item, index) => (
             <tr key={index}>
               <td>
-                <img src={item.productInfo.images} alt={item.productInfo.name} className="cart-image" />
+                <img
+                  src={item.productInfo.images}
+                  alt={item.productInfo.name}
+                  className="cart-image"
+                />
               </td>
               <td>{item.productInfo.name}</td>
               <td>
                 <div className="td-quantity">
-                <button onClick={() => props.removeItemFromCart(item.productInfo)} className="cart-button">-</button>
-                {item.quantity}
-                <button onClick={() => props.addItemToCart(item.productInfo)} className="cart-button">+</button>
+                  <button
+                    onClick={() => props.removeItemFromCart(item.productInfo)}
+                    className="cart-button"
+                  >
+                    -
+                  </button>
+                  {item.quantity}
+                  <button
+                    onClick={() => props.addItemToCart(item.productInfo)}
+                    className="cart-button"
+                  >
+                    +
+                  </button>
                 </div>
               </td>
               <td>${item.productInfo.price.toFixed(2)}</td>
@@ -43,7 +71,26 @@ export default function Cart(props) {
       <div className="cart-total">
         <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
       </div>
-      <button className="checkout-button">Checkout</button> {/* Add the checkout button */}
+      <button
+        className="checkout-button"
+        onClick={() => setCheckoutModalOpen(true)}
+      >
+        Checkout
+      </button>
+
+      {checkoutModalOpen && (
+        <div className="checkout-modal">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+          <button
+            className="close-modal-button"
+            onClick={() => setCheckoutModalOpen(false)}
+          >
+            Close
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ frontend_react/src/Components/Body/Cart/CheckoutForm.js
@@ -0,0 +1,37 @@
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+
+export function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+      // Handle payment processing on your server
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+}
